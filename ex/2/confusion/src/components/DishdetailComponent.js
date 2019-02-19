e07ed68a3cb838a4efa6cfb6218aaa746f710d93
Dinesh <dinesh.sinha@vidyo.com>
2019-02-18 22:08:46 -0500

    W2 Ex5

--- ex/2/confusion/src/components/DishdetailComponent.js
+++ ex/2/confusion/src/components/DishdetailComponent.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {
-    Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle
-} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({ dish }) {
 
@@ -68,14 +67,26 @@ const DishDetail = (props) => {
         selectedDish = props.dish
     }
     return (
-        <div className="row m-1">
-            <div className="col-sm-5 m-1">
-                <RenderDish dish={selectedDish} />
+        <div className="container">
+        <div className="row">
+            <Breadcrumb>
+
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
+        </div>
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
             </div>
-            <div className="col-sm-5 m-1">
-                <h4>Comments</h4>
-                <RenderComments dish={selectedDish} />
+            <div className="col-12 col-md-5 m-1">
+                <RenderComments comments={props.comments} />
             </div>
+        </div>
         </div>
     );
 
