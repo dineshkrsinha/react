06fba5b368c3db9f6fb37b1f064c52463d507659
Dinesh Sinha <dinesh.sinha@jet.com>
2019-03-03 13:09:44 -0500

    Before seperating into  separate component

--- ex/2/confusion/src/components/DishdetailComponent.js
+++ ex/2/confusion/src/components/DishdetailComponent.js
@@ -1,7 +1,101 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem,
+    Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label
+} from 'reactstrap';
 import { Link } from 'react-router-dom';
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>hello123456</div>
+        );
+    }
+}
+
+
+class RenderComments extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmitComment = this.handleSubmitComment.bind(this);
+    }
+
+    handleSubmitComment(event) {
+        /*this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);*/
+        event.preventDefault();
+
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    render() {
+
+        var comments = this.props.comments;
+
+        if (comments == null)
+            return (
+                <div></div>
+            );
+        const commentsLayout = comments.map((comment) => {
+            var formattedString =
+                new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)));
+
+            return (
+                <div className="row">
+                    <div className="row">
+                        <div className="col m-1">
+                            {comment.comment}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col m-1">
+                            ---{comment.author}, {formattedString}
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                {commentsLayout}
+                <CommentForm />
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+
+                    </ModalBody>
+                </Modal>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <Button outline onClick={this.toggleModal}><span className="fa fa-edit fa-lg"></span> Submit Comment</Button>
+                    </NavItem>
+                </Nav>
+            </div>
+        );
+
+    }
+}
+
+
+
 
 function RenderDish({ dish }) {
 
@@ -22,38 +116,8 @@ function RenderDish({ dish }) {
 
 }
 
-function RenderComments({ comments }) {
-    if (comments == null)
-        return (
-            <div></div>
-        );
-    const commentsLayout = comments.map((comment) => {
-        var formattedString =
-            new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)));
 
-        return (
-            <div className="row">
-                <div className="row">
-                    <div className="col m-1">
-                        {comment.comment}
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col m-1">
-                        ---{comment.author}, {formattedString}
-                    </div>
-                </div>
-            </div>
-        );
-    });
 
-    return (
-        <div className="container">
-            {commentsLayout}
-        </div>
-    );
-
-}
 
 const DishDetail = (props) => {
 
@@ -63,25 +127,25 @@ const DishDetail = (props) => {
     }
     return (
         <div className="container">
-        <div className="row">
-            <Breadcrumb>
+            <div className="row">
+                <Breadcrumb>
 
-                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-            </Breadcrumb>
-            <div className="col-12">
-                <h3>{props.dish.name}</h3>
-                <hr />
-            </div>                
-        </div>
-        <div className="row">
-            <div className="col-12 col-md-5 m-1">
-                <RenderDish dish={props.dish} />
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
             </div>
-            <div className="col-12 col-md-5 m-1">
-                <RenderComments comments={props.comments} />
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
             </div>
-        </div>
         </div>
     );
 
