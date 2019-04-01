a643737ac83cb0329729079a4209b3455e9fdf7a
Dinesh Sinha <dinesh.sinha@jet.com>
2019-03-31 23:33:03 -0400

    Assignment 4: part 2 complete

--- ex/2/confusion/src/components/MainComponent.js
+++ ex/2/confusion/src/components/MainComponent.js
@@ -12,7 +12,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
-import { postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
+import { postComment, postFeedback, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
@@ -28,14 +28,13 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
     postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
+    postFeedback: (firstname, lastname, telnum, email, agree, contactType, message) => dispatch(postFeedback(firstname, lastname, telnum, email, agree, contactType, message)),
     fetchDishes: () => { dispatch(fetchDishes()) },
     resetFeedbackForm: () => { dispatch(actions.reset('feedback')) },
     fetchComments: () => dispatch(fetchComments()),
     fetchPromos: () => dispatch(fetchPromos()),
     fetchLeaders: () => dispatch(fetchLeaders())
 });
-
-
 
 class Main extends Component {
 
@@ -95,7 +94,8 @@ class Main extends Component {
                             <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                             <Route path='/menu/:dishId' component={DishWithId} />
                             <Route exact path='/contactus' component={() => <Contact
-                                resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                                resetFeedbackForm={this.props.resetFeedbackForm}
+                                postFeedback={this.props.postFeedback} />} />
                             <Route exact path='/aboutus' component={() => <About
                                 leaders={this.props.leaders.leaders}
                                 leaderLoading={this.props.leaders.isLoading}
